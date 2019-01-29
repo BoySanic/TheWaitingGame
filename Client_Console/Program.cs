345750a87eae54510b77c373324e3deab42a3e56
boysanic <boysonicrevived@gmail.com>
2019-01-29 06:03:02 -0500

    Added client project

---
+++ Client_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
